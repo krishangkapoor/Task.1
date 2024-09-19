34a416dd3f9ac2dbba782beb60c75cb0c9c7bbc9
krishangkapoor <153663986+krishangkapoor@users.noreply.github.com>
2024-09-19 20:18:06 +0530

    urls.py

--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -20,7 +20,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# Swagger schema view configuration
 schema_view = get_schema_view(
    openapi.Info(
       title="TODO API",
@@ -40,7 +39,7 @@ urlpatterns = [
 
     # Swagger endpoints
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger UI
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # ReDoc UI (optional)
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # ReDoc UI 
 ]
 
 
