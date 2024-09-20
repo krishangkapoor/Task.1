cc2b9d9fd825a447f46547659fcf7163e6730832
krishangkapoor <153663986+krishangkapoor@users.noreply.github.com>
2024-09-20 17:56:28 +0530

    urls.py

--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -16,30 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="TODO API",
-      default_version='v1',
-      description="A simple TODO app API",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="support@todoapp.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  
-    path('api/', include('todo.urls')),  
+    path('admin/', admin.site.urls),
+    path('api/', include('todo.urls')),
 
-    # Swagger endpoints
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger UI
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # ReDoc UI 
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # Schema JSON
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),  # Redoc UI
 ]
-
-
