51fb05b6eb052f528e4d3798fe747c1715c15afe
krishangkapoor <153663986+krishangkapoor@users.noreply.github.com>
2024-09-26 12:31:37 +0530

    models.py

--- todo_project/todo/models.py
+++ todo_project/todo/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from .tasks import send_task_created_email
 
 class Todo(models.Model):
     task_name = models.CharField(max_length=255)
     is_done = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        send_task_created_email.delay(self.task_name)
 
     def __str__(self):
         return self.task_name
