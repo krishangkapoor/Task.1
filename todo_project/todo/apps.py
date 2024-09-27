922bec816a4eac8cbf1722ab7d531d63420a48d6
krishangkapoor <153663986+krishangkapoor@users.noreply.github.com>
2024-09-27 18:56:38 +0530

    apps.py

--- todo_project/todo/apps.py
+++ todo_project/todo/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
-
 class TodoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todo'
+
+    def ready(self):
+        from rest_framework import serializers
+        if not hasattr(serializers, 'NullBooleanField'):
+            serializers.NullBooleanField = serializers.BooleanField
