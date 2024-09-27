6d1b3cf4ff2adb4a4a62648239a8b4ce6decd7f7
krishangkapoor <153663986+krishangkapoor@users.noreply.github.com>
2024-09-27 18:06:40 +0530

    serializers.py

--- todo_project/todo/serializers.py
+++ todo_project/todo/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import Todo
 
-class TodoSerializer(serializers.ModelSerializer):
+class TodoReadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ['id', 'task_name', 'is_done']
+        fields = ['id', 'task_name', 'is_done']  
+
+class TodoWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['task_name']  
+
+class TodoUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['task_name', 'is_done']  
+
